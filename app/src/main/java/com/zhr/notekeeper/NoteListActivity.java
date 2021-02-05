178b1b34b266fe87a30d7d3842af8af3418a471d
Zachary Rzotkiewicz <zack.rzotkiewicz@harman.com>
2021-02-05 09:41:16 -0500

    Implemented drawer

--- app/src/main/java/com/zhr/notekeeper/NoteListActivity.java
+++ app/src/main/java/com/zhr/notekeeper/NoteListActivity.java
@@ -38,36 +38,16 @@ public class NoteListActivity extends AppCompatActivity {
         });
 
 
-    initializeDisplayContent();
+        initializeDisplayContent();
     }
 
     @Override
     protected void onResume(){
         super.onResume();
-//        mAdapterNotes.notifyDataSetChanged();
         mNoteRecyclerAdapter.notifyDataSetChanged();
     }
 
     private void initializeDisplayContent() {
-//        final ListView listNotes = findViewById(R.id.list_note);
-//
-//        List<NoteInfo> notes = DataManager.getInstance().getNotes();
-//        mAdapterNotes = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, notes);
-//
-//        listNotes.setAdapter(mAdapterNotes);
-//
-//        listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                Intent intent = new Intent(NoteListActivity.this, NoteActivity.class);
-//
-////                NoteInfo note = (NoteInfo) listNotes.getItemAtPosition(position);
-//                intent.putExtra(NoteActivity.NOTE_POSITION, position);
-//
-//                startActivity(intent);
-//            }
-//        });
-
         final RecyclerView recyclerNotes = (RecyclerView) findViewById(R.id.list_notes);
         final LinearLayoutManager notesLayoutManager = new LinearLayoutManager(this);
         recyclerNotes.setLayoutManager(notesLayoutManager);
