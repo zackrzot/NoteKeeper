420dcf9593297b29f27bfeb70b852cef8029a3cb
Zachary Rzotkiewicz <zack.rzotkiewicz@harman.com>
2021-02-05 16:57:26 -0500

    Implemented drawer test

--- app/src/androidTest/java/com/zhr/notekeeper/NextThroughNotesTest.java
+++ app/src/androidTest/java/com/zhr/notekeeper/NextThroughNotesTest.java
@@ -3,12 +3,17 @@ package com.zhr.notekeeper;
 import static org.junit.Assert.*;
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.contrib.DrawerActions;
+import androidx.test.espresso.contrib.NavigationViewActions;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.List;
+
 import static androidx.test.espresso.Espresso.*;
 import static androidx.test.espresso.action.ViewActions.*;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
@@ -25,6 +30,20 @@ public class NextThroughNotesTest {
     @Test
     public void NextThroughNotes(){
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_notes));
+
+        onView(withId(R.id.list_items)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+        int index = 0;
+        NoteInfo note = notes.get(index);
+
+        onView(withId(R.id.spinner_courses)).check(
+                matches(withSpinnerText(note.getCourse().getTitle())));
+        onView(withId(R.id.text_note_title)).check(matches(withText(note.getTitle())));
+        onView(withId(R.id.text_note_text)).check(matches(withText(note.getText())));
+
+
     }
 
 
